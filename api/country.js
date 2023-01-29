21bb027a1e3b96a1f3cd1f88bfeb7f1c78fe0357
Artem Nistuley <artem.nistuley@gmail.com>
2023-01-30 00:35:01 +0200

    Node app

---
+++ api/country.js
@@ -0,0 +1,12 @@
+const country = db('country');
+
+({
+  read(id) {
+    return country.read(id);
+  },
+
+  find(mask) {
+    const sql = 'SELECT * from country where name like $1';
+    return country.query(sql, [mask]);
+  }
+});
---
+++ api/user.js
@@ -0,0 +1,27 @@
+
+const users = db('users');
+
+({
+  read(id) {
+    return users.read(id, ['id', 'login']);
+  },
+
+  async create({ login, password }) {
+    const passwordHash = await hash(password);
+    return users.create({ login, password: passwordHash });
+  },
+
+  async update(id, { login, password }) {
+    const passwordHash = await hash(password);
+    return users.update(id, { login, password: passwordHash });
+  },
+
+  delete(id) {
+    return users.delete(id);
+  },
+
+  find(mask) {
+    const sql = 'SELECT login from users where login like $1';
+    return users.query(sql, [mask]);
+  },
+});
